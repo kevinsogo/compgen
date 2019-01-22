3b36e22200d98301e87ae59a44842d08e6ab8ed0
kcvajgf <kevin.charles.atienza@gmail.com>
2019-01-22 16:24:54 +0800

    Make gen_readme runnable anywhere (Tiny improvement)

--- readme_src/gen_readme.py
+++ readme_src/gen_readme.py
@@ -5,17 +5,16 @@ from __future__ import print_function
 import os.path
 from glob import glob
 
-# TODO improve this so it's not dependent on where it's run
-
-with open('README_TEMPLATE.md') as f:
+here = os.path.dirname(os.path.abspath(__file__))
+with open(os.path.join(here, 'README_TEMPLATE.md')) as f:
     s = f.read()
 
 # replace
-for pathname in sorted(glob('../examples/addition/*')):
+for pathname in sorted(glob(os.path.join(here, '..', 'examples', 'addition', '*'))):
     if os.path.isfile(pathname):
         with open(pathname) as f:
             s = s.replace('{{{' + os.path.basename(pathname) + '}}}', f.read().strip('\n'))
 
-with open('../README.md', 'w') as f:
+with open(os.path.join(here, '..', 'README.md'), 'w') as f:
     print("<!-- NOTE TO CONTRIBUTORS: PLEASE DON'T EDIT THIS FILE; EDIT readme_src/README_TEMPLATE.md INSTEAD, THEN RUN 'python2 gen_readme.py'. -->\n\n", file=f)
     f.write(s)
