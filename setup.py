1e7d7a4b54e8c6f88adade760c3868150708faf0
cjquines <cjquines@yahoo.com>
2019-02-20 18:55:33 +0800

    aux is a reserved name in windows

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(name='KompGen',
               'kg-polygon = kg:main_pg',
               'kg-hr = kg:main_hr',
               'kg-hackerrank = kg:main_hr',
-              'kg-aux = kg.script.aux:main',
+              'kg-aux = kg.script.auxillary:main',
           ],
       },
       packages=[
