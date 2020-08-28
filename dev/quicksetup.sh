867b6d70709f6e691e02820510a3d42203a95777
Kevin Atienza <kevin.charles.atienza@gmail.com>
2020-08-28 16:48:27 +0800

    add quick setup script (for dev)

---
+++ dev/quicksetup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# run as 'dev/quicksetup.sh'
+
+set -e
+
+# make scripts executable
+chmod +x setup.sh docs/src/makedocs
+
+python3 setup.py install
+python3 setup.py clean --all
+
+echo
+activate-global-python-argcomplete --user && echo "AUTOCOMPLETE READY" || echo "Skipping autocomplete"
+echo "DONE"
