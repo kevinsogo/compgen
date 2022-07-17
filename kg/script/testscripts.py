935d94126e9b86c78014e1957640ce20e13fe3d4
Kevin Atienza <kevin.charles.atienza@gmail.com>
2022-07-17 09:51:48 +0800

    Add start=0 to testscripts

--- kg/script/testscripts.py
+++ kg/script/testscripts.py
@@ -1,7 +1,9 @@
 from collections import defaultdict
 from itertools import islice, count
 from subprocess import PIPE
+import json
 import os.path
+import re
 
 from .formats import *
 from .programs import *
@@ -29,30 +31,47 @@ def parse_generator(name, *args, generators, relpath=None):
             return prog, args
 
 
+
 def run_testscript(inputs, testscript, generators, *, relpath=None):
     info_print("PARSING TESTSCRIPT")
-    filecount, gens = parse_testscript(testscript, generators, relpath=relpath)
+    filecount, gens, settings = parse_testscript(testscript, generators, relpath=relpath)
+
+    # get the subset of files we want to write
     files = list(islice(inputs, filecount))
+
     print(info_text("EXPECTING"), key_text(filecount), info_text("TEST FILES"))
     if len(files) < filecount:
         raise TestScriptError(f"{filecount} files needed but only {len(files)} input files found.")
+
+    file_for = dict(enumerate(files, settings['start']))
+
+    # print out the mapping of files for transparency
+    prep = 5
+    etced = False
+    for index, (target, tfile) in enumerate(file_for.items()):
+        if index < prep or index >= len(file_for.items()) - prep:
+            info_print(f'*{target:>6} --> {tfile}')
+        elif not etced:
+            etced = True
+            info_print('* ' + '.'*15)
 
     got_files = set()
     for gen, args, single, target, otarget, src_line in gens:
         if single:
             # single file, outputs to stdout
-            assert 1 <= target <= filecount
-            filename = files[target - 1]
+            filename = file_for[target]
             print(info_text(f'[o={otarget} t={target}] GENERATING'), key_text(filename),
                     info_text(f'(from {src_line!r})'))
             touch_container(filename)
             with open(filename, 'w') as file:
-                gen.do_compile().do_run(*args, stdout=file)
+                gen.do_compile().do_run(*args, time=True, label='GENERATOR', stdout=file)
             got_files.add(filename)
             yield filename
         else:
             # replace the first part
             info_print(f'[o={otarget}] GENERATING MULTIPLE: {len(target)} FILES (from {src_line!r})')
+
+            # TODO allow '$$' to appear anywhere
             dupseq, *rargs = args
             dupseq = ':' + dupseq
             args = dupseq, *rargs
@@ -68,11 +87,10 @@ def run_testscript(inputs, testscript, generators, *, relpath=None):
                         raise TestScriptError(f"Temp file {sfile} exists and is not a file! Please clear {temp_folder}")
                     os.remove(sfile)
 
-            gen.do_compile().do_run(*args)
+            gen.do_compile().do_run(*args, time=True, label='GENERATOR')
             assert len(sfilenames) == len(target)
             for sfile, t in zip(sfilenames, target):
-                assert 1 <= t <= len(files)
-                tfile = files[t - 1]
+                tfile = file_for[t]
                 print(info_text(f"[o={otarget} t={t}] Moving {sfile} to"), key_text(tfile))
                 touch_container(tfile)
                 if os.path.exists(tfile): os.remove(tfile)
@@ -83,16 +101,17 @@ def run_testscript(inputs, testscript, generators, *, relpath=None):
     assert got_files == set(files)
 
 
-def parse_testscript(testscript, generators, *, relpath=None):
+SETTING_RE = re.compile(r'^[a-zA-Z][a-zA-Z0-9_]*\Z')
+def parse_testscript(testscript, generators, *, relpath=None, **kwargs):
     found = set()
-    def mex(start=1):
-        return next(v for v in count(start) if v not in found)
+    def mex():
+        return next(v for v in count(settings['start']) if v not in found)
 
     def validate_target(index):
         if index in found:
             raise TestScriptError(f"Duplicate target index found: {index}")
-        if index < 1:
-            raise TestScriptError(f"Target index must be positive. Got {index}")
+        if index < settings['start']:
+            raise TestScriptError(f"Target index must be at least start={settings['start']}. Got {index}")
         found.add(index)
 
     gens = {}
@@ -103,13 +122,65 @@ def parse_testscript(testscript, generators, *, relpath=None):
             warn_print(f'WARNING: Testscript line {line!r} will generate the same file/s as '
                        f'{found_gen_args[gen_args][-1]!r}. (Random seed is determined by args)')
         found_gen_args[gen_args].append(line)
-    later = []
+
+    settings = {
+        'start': 1,
+    }
+    settings_types = {
+        'start': int,
+    }
+    def parse_assignment(line):
+        var, delim, val = line.partition('=')
+        if delim:
+            assert delim == '='
+            if SETTING_RE.fullmatch(var):
+                return var, val
+
+    def assign(var, val, *, parse_from_str=False):
+
+        if parse_from_str:
+            info_print(f"Testscript setting {var} = {val!r} (to be parsed)")
+            try:
+                val = json.loads(val)
+            except json.JSONDecodeError:
+                raise TestScriptError(f"Cannot parse {val!r}")
+
+        info_print(f"Testscript setting {var} = {val!r}")
+
+        if var not in settings:
+            raise TestScriptError(f"Unknown setting: {var}")
+        var_type = settings_types[var]
+
+        if not isinstance(val, var_type):
+            raise TestScriptError(f"Cannot convert {val!r} to {var_type}")
+
+        settings[var] = val
+
+    genlines = []
     for line in testscript.strip().split('\n'):
-        parts = line.split()
-        if not parts or parts[0] == '#':
+        line = line.strip()
+        if not line or line.startswith('#'):
             continue
+
+        # TODO use walrus
+        assignment = parse_assignment(line)
+        if assignment:
+            assign(*assignment, parse_from_str=True)
+        else:
+            genlines.append(line)
+
+    # assign
+    for key, val in kwargs.items():
+        assign(key, val)
+
+    info_print(f"Testscript settings parsed:")
+    for var, val in settings.items():
+        info_print(f"* using setting {var} = {val!r}")
+
+    later = []
+    for line in genlines:
         try:
-            prog, *args, pipe, target = parts
+            prog, *args, pipe, target = line.split()
         except ValueError as exc:
             raise TestScriptError(f"Invalid testscript line: {line!r}")
 
@@ -165,7 +236,7 @@ def parse_testscript(testscript, generators, *, relpath=None):
             if target == "$$":
                 count_args = ['COUNT'] + rargs
                 info_print(f"Running the multifile generator {gen.filename} on {' '.join(count_args)!r} once to get the file count...")
-                result = gen.do_compile().do_run(*count_args, stdout=PIPE)
+                result = gen.do_compile().do_run(*count_args, time=True, label='GENERATOR', stdout=PIPE)
                 nfiles = int(result.stdout.decode('utf-8'))
                 info_print(f"Ran the multifile generator {gen.filename} on {' '.join(count_args)!r} once to get the file count...got {nfiles} files.")
             else:
@@ -185,17 +256,25 @@ def parse_testscript(testscript, generators, *, relpath=None):
             args = rtarget, *rargs
             gens[min(indices)] = gen, args, False, indices, target, src_line
 
-    if not all(i == target for i, target in enumerate(sorted(found), 1)):
-        raise TestScriptError("Some test files missing from the sequence. They must generate 1, 2, 3, ...")
+    expected_seq = lambda: count(settings['start'])
+    if sorted(found) != [*islice(expected_seq(), len(found))]:
+        expected = ', '.join(map(str, islice(expected_seq(), 4)))
+        raise TestScriptError(f"Some test files missing from the sequence. They must generate {expected}, ...")
 
-    return len(found), [value for key, value in sorted(gens.items())]
+    return len(found), [value for key, value in sorted(gens.items())], settings
 
 
-def convert_testscript(testscript, generators, *, relpath=None):
-    filecount, gens = parse_testscript(testscript, generators, relpath=relpath)
+def convert_testscript(testscript, generators, *, relpath=None, start=1):
+    filecount, gens, settings = parse_testscript(testscript, generators, relpath=relpath)
+    offset = start - settings['start']
+    info_print(f"Testscript starts at {settings['start']}, target starts at {start}, offset is {offset}")
 
     for gen, args, single, target, otarget, src_line in gens:
         if src_line[0] == '!':
             warn_print(f"Warning: The following testscript line cannot be added to Polygon: '{src_line}'. Add it manually.")
         else:
-            yield ' '.join([os.path.splitext(os.path.basename(gen.filename))[0], *args, '>', otarget])
+            if single:
+                starget = str(target + offset)
+            else:
+                starget = '{' + compress_t_sequence(','.join(str(t + offset) for t in target)) + '}'
+            yield ' '.join([os.path.splitext(os.path.basename(gen.filename))[0], *args, '>', starget])
